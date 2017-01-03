d7f47e89fcfc9dccbc17438b4ce07a94b7dffea7
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2017-01-03 10:01:20 -0600

    godoc

--- go/forkbomb/cmd/forkbomb/main.go
+++ go/forkbomb/cmd/forkbomb/main.go
@@ -1,5 +1,8 @@
+// Package main provides a forkbomb executable.
 package main
 
+// Main recursively forks itself, without a basecase.
+// Monitor resources, and preferably run in a virtualized or isolated environment.
 func main() {
 	for {
 		go main()
